ebd46d7d55a15c110bf21a5309fc34224f759c74
maximBelitski <maxim.belitski@gmail.com>
2021-03-17 18:11:09 +0300

    fix linting

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from app.models import Post
 
+
 class PostTestCase(TestCase):
     def testPost(self):
         post = Post(title="My title", description="Blurb", wiki="Post Body")
